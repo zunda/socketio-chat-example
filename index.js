55e0233283bf07abbd03bf3eb81a9546b6cd4aa2
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-09-08 09:52:41 +0200

    feat: initial commit for step #2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path');
 
 const app = express();
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(3000, () => {
