0d827926acb9f6ee8e847c41e4bac8393aac1215
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-09-08 09:57:18 +0200

    feat: initial commit for step #5

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
