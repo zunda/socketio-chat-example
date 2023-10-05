5d7a43a1c57d359e45a050cc8b1b792a68c9ee58
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-10-05 15:50:43 +0200

    feat: initial commit for step #7

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
@@ -12,10 +27,30 @@ app.get('/', (req, res) => {
   res.sendFile(new URL('./index.html', import.meta.url).pathname);
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+io.on('connection', async (socket) => {
+  socket.on('chat message', async (msg) => {
+    let result;
+    try {
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    io.emit('chat message', msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(3000, () => {
